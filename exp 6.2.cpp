7e586ecc7a7127bb0bd8303ae3d02413ed9ebc9d
sehswag <sujalkamble1610@gmail.com>
2025-11-04 11:41:58 +0530

    Update exp 6.2.cpp

--- exp 6.2.cpp
+++ exp 6.2.cpp
@@ -17,10 +17,11 @@ public:
 int main()
  {
 	
-    cout << "Name: Shubhada Tarlekar\n";
+    cout << "Name: Sujal Kambale\n";
     cout << "Div: B\n";
-    cout << "Roll no.: 95\n";
+    cout << "Roll no.: 105\n";
     Car obj1;
     Bus obj2;
     return 0;
 }
+
